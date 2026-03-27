0a8c69c22f243d861ac74e232d47487322b2d761
BugBananaLiver <Melissahaertzen@gmail.com>
2026-03-26 20:22:20 -0400

    Completed 1st challenge

---
+++ Reverse_A_String.py
@@ -0,0 +1,14 @@
+#def reverse_string(string):
+    #print(string[::-1])
+    #return
+
+#reverse_string("hello")
+#reverse_string("Python")
+
+def reverse_string(string):
+    for character in string:
+        print(character, end= " ")
+        return
+    
+reverse_string("hello")
+    
